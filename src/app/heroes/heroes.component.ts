894117cef8be6e8fb47c3543704b86db76fae62a
matvan99it <vannicraft@gmail.com>
2019-05-02 17:38:12 +0200

    1 file modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
