aa1ebc4ceca09aa0b30adc8beb7c0450fc76290c
matvan99it <vannicraft@gmail.com>
2019-05-02 17:50:32 +0200

    1 file modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,13 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from '../heroes/heroes.component';
 
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
   exports: [ RouterModule ],
   imports: [ RouterModule.forRoot(routes) ],
+  
 })
 
 export class AppRoutingFlatModule { }
