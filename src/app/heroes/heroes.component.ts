74954431110f786532bfbd01c458fc246a7d1b64
matvan99it <vannicraft@gmail.com>
2019-05-02 17:22:49 +0200

    2 files modified, 1 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,18 +12,20 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void 
   {
     this.selectedHero = hero;
   }
-  constructor() { 
+  constructor(private heroService: HeroService) { 
     
   }
-
+getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
   ngOnInit() {
-    
+    this.getHeroes();
   }
 
 }
