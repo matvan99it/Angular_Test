906939ff4466c47ba42c9865964eda2c33ff09d7
matvan99it <vannicraft@gmail.com>
2019-05-03 09:21:49 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
- 
+ import { HttpClientModule }    from '@angular/common/http';
+
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
