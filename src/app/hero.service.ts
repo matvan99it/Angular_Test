4440dddbbaa73f516e31f4d40ea864e2882d4efc
matvan99it <vannicraft@gmail.com>
2019-05-03 09:40:48 +0200

    1 file modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -32,11 +32,14 @@ getHeroes (): Observable<Hero[]> {
     );
 }
  
-  getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
-  }
+/** GET hero by id. Will 404 if id not found */
+getHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
+}
 
   private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
@@ -61,6 +64,7 @@ private handleError<T> (operation = 'operation', result?: T) {
     return of(result as T);
   };
 }
+
 
 
 }
