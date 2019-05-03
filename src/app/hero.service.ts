824ccb0fa018c241d44979b7b3e8d7e111d9e6bc
matvan99it <vannicraft@gmail.com>
2019-05-03 09:44:59 +0200

    3 files modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -78,6 +78,12 @@ updateHero (hero: Hero): Observable<any> {
   );
 }
 
-
+/** POST: add a new hero to the server */
+addHero (hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
 
 }
